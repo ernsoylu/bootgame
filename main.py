4c64c0c8fb3a14a7e25d01b75878c56c8ef4ecb1
ernsoylu <ernsoylu@gmail.com>
2025-07-30 10:41:40 +0100

    Player feature added

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
+        
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+
+        player1 = Player(x, y)
+        player1.draw(screen)
+
         pygame.display.flip()
+
+
+        #limit the framerate
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
