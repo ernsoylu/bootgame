8f7d12a31e9bacb475e704a0aed0dc3fa3d378b9
ernsoylu <ernsoylu@gmail.com>
2025-07-30 22:15:12 +0100

    Player functionality added

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player1 = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
         
-        x = SCREEN_WIDTH / 2
-        y = SCREEN_HEIGHT / 2
-
-        player1 = Player(x, y)
+        player1.update(dt)
+        
+        screen.fill("black")
         player1.draw(screen)
-
         pygame.display.flip()
-
 
         #limit the framerate
         dt = clock.tick(60)/1000
