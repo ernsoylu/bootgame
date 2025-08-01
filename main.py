592a10c43f9857cad3657594eee94fb699b3e9df
ernsoylu <ernsoylu@gmail.com>
2025-08-01 14:19:32 +0100

    Collision mechanism added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
@@ -10,17 +13,29 @@ def main():
     
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player1 = Player(x, y)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        player1.update(dt)
-        
         screen.fill("black")
-        player1.draw(screen)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collide(player1):
+                print("Game Over!")
+                return
+        for instance in drawable:
+            instance.draw(screen)
+        
         pygame.display.flip()
 
         #limit the framerate
